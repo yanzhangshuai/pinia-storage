fe714c5682ad71257619c3874cbd001d12a6ffac
mowenjinzhao <yanzhangshuai@126.com>
2022-04-26 18:34:59 +0800

    style: 添加eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
-import { createPinia, defineStore, setActivePinia } from "pinia"
-import { PiniaStorage } from './index';
+import { createPinia, defineStore, setActivePinia } from 'pinia'
+import { PiniaStorage } from './index'
 
-const delay = (t?: number) => new Promise((r) => setTimeout(r, t))
+const delay = (t?: number) => new Promise(resolve => setTimeout(resolve, t))
 
 interface Storage {
   bar?: string
@@ -19,145 +19,140 @@ const storage1Store = defineStore<'storage1', Storage>('storage1', {
     session: {
       baz: {
         read: (baz) => {
-          return Number(baz);
-        }
+          return Number(baz)
+        },
       },
       foo: {
         write: (foo: object) => {
-          return JSON.stringify(foo);
+          return JSON.stringify(foo)
         },
         read: (foo) => {
           try {
-            if (!foo) return null;
-            return JSON.parse(foo);
-          } catch (error) {
+            if (!foo)
+              return null
+            return JSON.parse(foo)
+          }
+          catch (error) {
             return null
           }
-        }
+        },
       },
     },
     local: ['bar'],
-  }
-});
+  },
+})
 
 const storage2Store = defineStore<'storage2', Storage>('storage2', {
   state: () => ({ bar: '', baz: 0, foo: null }),
   storage: {
-    'local': {
-      'baz': {
+    local: {
+      baz: {
         read: (baz) => {
-          return Number(baz);
-        }
+          return Number(baz)
+        },
       },
-      'foo': {
+      foo: {
         write: (foo: object) => {
-          return JSON.stringify(foo);
+          return JSON.stringify(foo)
         },
         read: (foo) => {
           try {
-            if (!foo) return null;
-            return JSON.parse(foo);
-          } catch (error) {
+            if (!foo)
+              return null
+            return JSON.parse(foo)
+          }
+          catch (error) {
             return null
           }
-        }
+        },
       },
     },
-    'session': ['bar'],
-  }
-});
-
+    session: ['bar'],
+  },
+})
 
 describe('storage write', () => {
-
   beforeEach(() => {
     const pinia = createPinia()
-    //@ts-ignore
+    // @ts-expect-error 无法使用use
     pinia._p.push(PiniaStorage())
     setActivePinia(pinia)
   })
 
-
-
   it('example', async () => {
-    const store1 = storage1Store();
-    const store2 = storage2Store();
+    const store1 = storage1Store()
+    const store2 = storage2Store()
 
-    store1.bar = 'store1_bar';
-    store2.bar = 'store2_bar';
-    store1.baz = 1;
-    store2.baz = 2;
+    store1.bar = 'store1_bar'
+    store2.bar = 'store2_bar'
+    store1.baz = 1
+    store2.baz = 2
 
-    const obj1 = { bar: 1, baz: '1', foo: true };
-    const obj2 = { bar: 2, baz: '2', foo: false };
+    const obj1 = { bar: 1, baz: '1', foo: true }
+    const obj2 = { bar: 2, baz: '2', foo: false }
 
-    store1.foo = obj1;
-    store2.foo = obj2;
+    store1.foo = obj1
+    store2.foo = obj2
 
-    await delay();
+    await delay()
 
-    expect(window.sessionStorage.getItem(store1.$id + '_foo')).toBe(JSON.stringify(obj1));
-    expect(window.sessionStorage.getItem(store1.$id + '_baz')).toBe('1');
-    expect(window.localStorage.getItem(store1.$id + '_bar')).toBe('store1_bar');
+    expect(window.sessionStorage.getItem(`${store1.$id}_foo`)).toBe(JSON.stringify(obj1))
+    expect(window.sessionStorage.getItem(`${store1.$id}_baz`)).toBe('1')
+    expect(window.localStorage.getItem(`${store1.$id}_bar`)).toBe('store1_bar')
 
-    expect(window.sessionStorage.getItem(store2.$id + '_bar')).toBe('store2_bar');
-    expect(window.localStorage.getItem(store2.$id + '_foo')).toBe(JSON.stringify(obj2));
-    expect(window.localStorage.getItem(store2.$id + '_baz')).toBe('2');
-  });
+    expect(window.sessionStorage.getItem(`${store2.$id}_bar`)).toBe('store2_bar')
+    expect(window.localStorage.getItem(`${store2.$id}_foo`)).toBe(JSON.stringify(obj2))
+    expect(window.localStorage.getItem(`${store2.$id}_baz`)).toBe('2')
+  })
 })
 
 describe('storage write prefix', () => {
-
   beforeEach(() => {
     const pinia = createPinia()
-    //@ts-ignore
+    // @ts-expect-error 无法使用use
     pinia._p.push(PiniaStorage({ prefix: 'storage-' }))
     setActivePinia(pinia)
   })
 
   it('example', async () => {
-    const store1 = storage1Store();
-    store1.bar = 'store1_bar';
-    store1.baz = 1;
-    const obj1 = { bar: 1, baz: '1', foo: true };
+    const store1 = storage1Store()
+    store1.bar = 'store1_bar'
+    store1.baz = 1
+    const obj1 = { bar: 1, baz: '1', foo: true }
 
-    store1.foo = obj1;
+    store1.foo = obj1
 
-    await delay();
+    await delay()
 
-    expect(window.localStorage.getItem('storage-' + store1.$id + '_bar')).toBe('store1_bar');
-    expect(window.sessionStorage.getItem('storage-' + store1.$id + '_foo')).toBe(JSON.stringify(obj1));
-    expect(window.sessionStorage.getItem('storage-' + store1.$id + '_baz')).toBe('1');
-
-  });
+    expect(window.localStorage.getItem(`storage-${store1.$id}_bar`)).toBe('store1_bar')
+    expect(window.sessionStorage.getItem(`storage-${store1.$id}_foo`)).toBe(JSON.stringify(obj1))
+    expect(window.sessionStorage.getItem(`storage-${store1.$id}_baz`)).toBe('1')
+  })
 })
 
-
 describe('storage read', () => {
-
   beforeEach(() => {
     const pinia = createPinia()
-    //@ts-ignore
+    // @ts-expect-error 无法使用use
     pinia._p.push(PiniaStorage())
     setActivePinia(pinia)
   })
 
   it('example', async () => {
-    const store1 = storage1Store();
-    const store2 = storage2Store();
+    const store1 = storage1Store()
+    const store2 = storage2Store()
 
-    const obj1 = { bar: 1, baz: '1', foo: true };
-    const obj2 = { bar: 2, baz: '2', foo: false };
+    const obj1 = { bar: 1, baz: '1', foo: true }
+    const obj2 = { bar: 2, baz: '2', foo: false }
 
-    await delay();
+    await delay()
 
-    expect(store1.bar).toBe('store1_bar');
-    expect(store1.baz).toBe(1);
-    expect(store1.foo).toEqual(obj1);
+    expect(store1.bar).toBe('store1_bar')
+    expect(store1.baz).toBe(1)
+    expect(store1.foo).toEqual(obj1)
 
-    expect(store2.bar).toBe('store2_bar');
-    expect(store2.baz).toBe(2);
+    expect(store2.bar).toBe('store2_bar')
+    expect(store2.baz).toBe(2)
     expect(store2.foo).toEqual(obj2)
-
-  });
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,124 @@
-import { PiniaPluginContext, StateTree, Store } from 'pinia';
+import type { PiniaPluginContext, StateTree, Store } from 'pinia'
 
-
-export const PiniaStorage =
-  (options?: { prefix?: string }) =>
+export const PiniaStorage
+  = (options?: { prefix?: string }) =>
     ({ options: piniaOptions, store }: PiniaPluginContext) => {
+      initStore(store, piniaOptions?.storage, options?.prefix)
+      watchStore(store, piniaOptions?.storage, options?.prefix)
+    }
 
-      initStore(store, piniaOptions?.storage, options?.prefix);
-      watchStore(store, piniaOptions?.storage, options?.prefix);
-
-    };
-
-
-type StorageConvertType = {
+interface StorageConvertType {
   write?: (...args: any[]) => string
   read?: (args: string | null) => any
 }
 
-export type PiniaStorage = {
-  session?: Array<keyof StateTree> | Partial<Record<keyof StateTree, StorageConvertType>>,
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export interface PiniaStorage {
+  session?: Array<keyof StateTree> | Partial<Record<keyof StateTree, StorageConvertType>>
   local?: Array<keyof StateTree> | Partial<Record<keyof StateTree, StorageConvertType>>
-};
+}
 
-type StorageType = 'local' | 'session';
+type StorageType = 'local' | 'session'
 
 const Storage = {
   local: window.localStorage,
-  session: window.sessionStorage
-};
+  session: window.sessionStorage,
+}
 
 function watchStore(store: Store<string, Record<string | number | symbol, any>>, piniaStorage?: PiniaStorage, prefix?: string) {
+  if (!store || !piniaStorage)
+    return
 
-  if (!store || !piniaStorage) return;
-
-  prefix = prefix || '';
+  prefix = prefix || ''
 
   const _func = (storageName: StorageType) => {
+    const currentPiniaStorage = piniaStorage?.[storageName]
+    const currentStorage = Storage?.[storageName]
 
-    const currentPiniaStorage = piniaStorage?.[storageName];
-    const currentStorage = Storage?.[storageName];
+    if (!currentPiniaStorage || !currentStorage)
+      return
 
-    if (!currentPiniaStorage || !currentStorage) return;
-
-    if (!isArray(currentPiniaStorage) && !isObject(currentPiniaStorage)) return;
+    if (!isArray(currentPiniaStorage) && !isObject(currentPiniaStorage))
+      return
 
     store.$subscribe((item, state) => {
-
-      const updateKeys = Object.keys(state);
+      const updateKeys = Object.keys(state)
 
       if (isArray(currentPiniaStorage)) {
-
         intersection(currentPiniaStorage, updateKeys).forEach((key) => {
-
           // 利用交集获取当前需要更新的key
-          currentStorage.setItem(`${prefix}${item.storeId}_${key.toString()}`, state[key]);
-
-        });
-
-      } else {
+          currentStorage.setItem(`${prefix}${item.storeId}_${key.toString()}`, state[key])
+        })
+      }
+      else {
         // Object
 
-        const storageKeys = Object.keys(currentPiniaStorage);
+        const storageKeys = Object.keys(currentPiniaStorage)
 
         intersection(storageKeys, updateKeys).forEach((key) => {
           // 利用交集获取当前需要更新的key
 
-          const write = currentPiniaStorage[key]?.write;
+          const write = currentPiniaStorage[key]?.write
           // 转换
-          const value = write ? write(state[key]) : state[key].toString();
+          const value = write ? write(state[key]) : state[key].toString()
 
-          currentStorage.setItem(`${prefix}${item.storeId}_${key.toString()}`, value);
-        });
+          currentStorage.setItem(`${prefix}${item.storeId}_${key.toString()}`, value)
+        })
       }
+    })
+  }
 
-    });
+  _func('local')
 
-
-  };
-
-  _func('local');
-
-  _func('session');
-
+  _func('session')
 }
 
 function initStore(store: Store<string, Record<string | number | symbol, any>>, piniaStorage?: PiniaStorage, prefix?: string) {
+  if (!store || !piniaStorage)
+    return
 
-  if (!store || !piniaStorage) return;
-
-  prefix = prefix || '';
+  prefix = prefix || ''
 
   const _func = (storageName: StorageType) => {
+    const currentPiniaStorage = piniaStorage?.[storageName]
+    const currentStorage = Storage?.[storageName]
 
-    const currentPiniaStorage = piniaStorage?.[storageName];
-    const currentStorage = Storage?.[storageName];
-
-    if (!currentPiniaStorage || !currentStorage) return;
-
+    if (!currentPiniaStorage || !currentStorage)
+      return
 
     if (isArray(currentPiniaStorage)) {
       // Array
-      currentPiniaStorage.forEach(key => {
-
-        store[key] = currentStorage.getItem(`${prefix}${store.$id}_${key.toString()}`);
-      });
-
-    } else if (isObject(currentPiniaStorage)) {
-      // Object
-      Object.keys(currentPiniaStorage).forEach(key => {
-
-        const value = currentStorage.getItem(`${prefix}${store.$id}_${key.toString()}`);
-        // 转换
-        store[key] = currentPiniaStorage[key]?.read ? currentPiniaStorage?.[key]?.read?.(value) : value;
-      });
+      currentPiniaStorage.forEach((key) => {
+        store[key] = currentStorage.getItem(`${prefix}${store.$id}_${key.toString()}`)
+      })
     }
-  };
+    else if (isObject(currentPiniaStorage)) {
+      // Object
+      Object.keys(currentPiniaStorage).forEach((key) => {
+        const value = currentStorage.getItem(`${prefix}${store.$id}_${key.toString()}`)
+        // 转换
+        store[key] = currentPiniaStorage[key]?.read ? currentPiniaStorage?.[key]?.read?.(value) : value
+      })
+    }
+  }
 
-  _func('local');
-  _func('session');
-
-
+  _func('local')
+  _func('session')
 }
 
-
-const toString = Object.prototype.toString;
+const toString = Object.prototype.toString
 
 function is(val: unknown, type: string): boolean {
-  return toString.call(val) === `[object ${type}]`;
+  return toString.call(val) === `[object ${type}]`
 }
 
 function isObject(val: any): val is Record<string | number | symbol, unknown> {
-  return val !== null && is(val, 'Object');
+  return val !== null && is(val, 'Object')
 }
-
 
 function isArray(val: any): val is Array<any> {
-  return val && is(val, 'Array');
+  return val && is(val, 'Array')
 }
-
 
 /**
  * 交集
@@ -146,15 +128,16 @@ function isArray(val: any): val is Array<any> {
  * @returns
  */
 function intersection<T>(arr1: Array<T>, arr2: Array<T>, fn?: (val1: T, val2: T) => boolean) {
-  const _fn = fn || ((val1, val2) => val1 === val2);
-  return arr1.filter((val1) => arr2.find((val2) => _fn(val1, val2)));
+  const _fn = fn || ((val1, val2) => val1 === val2)
+  return arr1.filter(val1 => arr2.find(val2 => _fn(val1, val2)))
 }
 
 declare module 'pinia' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export interface DefineStoreOptionsBase<S, Store> {
     storage?: {
-      session?: Partial<Record<keyof S, StorageConvertType>> | Array<keyof S>;
-      local?: Partial<Record<keyof S, StorageConvertType>> | Array<keyof S>;
-    };
+      session?: Partial<Record<keyof S, StorageConvertType>> | Array<keyof S>
+      local?: Partial<Record<keyof S, StorageConvertType>> | Array<keyof S>
+    }
   }
 }
